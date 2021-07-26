8e8fcfd27bca2489e4ab49632a07ae07c5272e77
XIAYM <66472712+XIAYM-gh@users.noreply.github.com>
2021-07-26 09:43:04 +0800

    优化

--- 源码/TouZi.java
+++ 源码/TouZi.java
@@ -4,29 +4,25 @@ import com.windowx.miraibot.plugin.Plugin;
 import com.windowx.miraibot.utils.LogUtil;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.message.data.At;
-
 public class TouZi extends Plugin {
-				String PluginName = "TouZi";
-				@Override public void onEnable(){
-				loginfo("骰子插件已加载!");
-				}
-
-				@Override public void onDisable(){
-				loginfo("好家伙我直接正道的光");
-				loginfo("拒绝赌博从我做起");
-				loginfo("骰子插件爬了");
-				}
-
-				@Override public void onGroupMessage(GroupMessageEvent event){
-				String msg = event.getMessage().contentToString();
-				if(msg.equals("#qwq touzi")){
-																				Random r = new Random();
-																				int randomint = r.nextInt(6) + 1;
-																				event.getGroup().sendMessage(new At(event.getSender().getId()) .plus("宁摇到了: "+randomint));
-				}
-				}
-
-				public void loginfo(String logmsg){
-								LogUtil.log("["+PluginName+"] "+logmsg);
-				}
+	String PluginName = "TouZi";
+	@Override public void onEnable(){
+		loginfo("骰子插件已加载!");
+	}
+	@Override public void onDisable(){
+		loginfo("好家伙我直接正道的光");
+		loginfo("拒绝赌博从我做起");
+		loginfo("骰子插件爬了");
+	}
+	@Override public void onGroupMessage(GroupMessageEvent event){
+		String msg = event.getMessage().contentToString();
+		if(msg.equals("#qwq touzi")){
+			Random r = new Random();
+			int randomint = r.nextint(6) + 1;
+			event.getGroup().sendMessage(new At(event.getSender().getId()) .plus("宁摇到了: "+randomint));
+		}
+	}
+	public void loginfo(String logmsg){
+		LogUtil.log("["+PluginName+"] "+logmsg);
+	}
 }
